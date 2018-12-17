83e9af77a94810350c0b2bcefbc803286d01a974
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-12-16 22:18:57 -0500

    not withInfo

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,14 +1,13 @@
-import { configure, addDecorator } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
-import { withKnobs } from '@storybook/addon-knobs';
+import { configure, addDecorator } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+import { withKnobs } from "@storybook/addon-knobs";
 
-const req = require.context('../src/stories', true, /.stories.tsx$/);
+const req = require.context("../src/stories", true, /.stories.tsx$/);
 
 function loadStories() {
   req.keys().forEach(req);
 }
 
-addDecorator(withInfo());
 addDecorator(withKnobs);
 
 configure(loadStories, module);

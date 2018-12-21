b77cb894b6f8a214dcc863a5dcfcd91b83d4d797
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-12-20 22:22:20 -0500

    Malakies

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,5 @@ export { Button } from "./Button/button";
 export { Input } from "./Input/input";
 export { IconButton } from "./IconButton/iconButton";
 export { Checkbox } from "./Checkbox/checkbox";
+export { Switch } from "./Switch/switch";
+export { Tag } from "./Tag/tag";
